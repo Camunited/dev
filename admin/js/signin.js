50f90b36d50e21147ce6263f3ac14432d1063fd6
Austin Mattix <songchimchek23712@gmail.com>
2022-05-13 06:30:23 -0400

    New

--- admin/js/signin.js
+++
@@ -1,9 +0,0 @@
-function showPassword() {
-  var x = document.getElementById("floatingPassword");
-  if (x.type === "password") {
-    x.type = "text";
-  } else {
-    x.type = "password";
-  }
-}
-// $('.your-checkbox').prop('indeterminate', true);
